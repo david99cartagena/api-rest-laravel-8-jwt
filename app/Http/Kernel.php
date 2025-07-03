179425e40eb043437113810482cf6695e88a267a
david99cartagena <32483852+david99cartagena@users.noreply.github.com>
2025-07-02 22:22:39 -0500

    prueba para swagger

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -37,8 +37,8 @@ class Kernel extends HttpKernel
             \Illuminate\View\Middleware\ShareErrorsFromSession::class,
             \App\Http\Middleware\VerifyCsrfToken::class,
             \Illuminate\Routing\Middleware\SubstituteBindings::class,
-            \App\Http\Middleware\IsAdmin::class,
-            \App\Http\Middleware\IsUserAuth::class,
+            // \App\Http\Middleware\IsAdmin::class,
+            // \App\Http\Middleware\IsUserAuth::class,
         ],
 
         'api' => [
