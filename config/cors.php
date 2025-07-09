2e843535129036b5e15a43b9591f0449166faa87
david99cartagena <32483852+david99cartagena@users.noreply.github.com>
2025-07-09 09:34:20 -0500

    ajuste de cors

--- config/cors.php
+++ config/cors.php
@@ -19,7 +19,7 @@ return [
 
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => ['https://api-rest-laravel-8-jwt.onrender.com'],
+    'allowed_origins' => ['*'],
 
     'allowed_origins_patterns' => [],
 
