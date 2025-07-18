3e6f1062c76331e628945707c6f5168ca7ffee61
david99cartagena <32483852+david99cartagena@users.noreply.github.com>
2025-07-18 16:00:52 -0500

    ajuste de l5 swagger

--- config/l5-swagger.php
+++ config/l5-swagger.php
@@ -95,7 +95,7 @@ return [
             /*
              * Edit to set the api's base path
              */
-            'base' => env('L5_SWAGGER_CONST_HOST', 'http://localhost:8000'),
+            'base' => env('L5_SWAGGER_CONST_HOST', 'http://localhost'),
 
             /*
              * Absolute path to directories that should be excluded from scanning
@@ -323,7 +323,7 @@ return [
          * Constants which can be used in annotations
          */
         'constants' => [
-            'L5_SWAGGER_CONST_HOST' => env('L5_SWAGGER_CONST_HOST', 'http://localhost:8000'),
+            'L5_SWAGGER_CONST_HOST' => env('L5_SWAGGER_CONST_HOST', 'http://localhost'),
         ],
     ],
 ];
